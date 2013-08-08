6a5704516023c34af8f5d7495647f816c2c504b3
sscpro <ssc@veda.net>
2013-08-08 16:14:26 +0200

    added database type mapping for DB2 for IBM i (version 7.1)

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/cfg/ProcessEngineConfigurationImpl.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/cfg/ProcessEngineConfigurationImpl.java
@@ -516,6 +516,7 @@ public abstract class ProcessEngineConfigurationImpl extends ProcessEngineConfig
     databaseTypeMappings.setProperty("DB2/SUN64","db2");
     databaseTypeMappings.setProperty("DB2/PTX","db2");
     databaseTypeMappings.setProperty("DB2/2","db2");
+    databaseTypeMappings.setProperty("DB2 UDB for AS/400", "db2i");
     return databaseTypeMappings;
   }
 
